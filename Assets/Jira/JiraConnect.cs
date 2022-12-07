7169ea8463fae51d4cdbc194c7585a1fb37b9514
Adam-McNeil <adam.mcneil@oasisdigital.com>
2022-12-07 12:38:25 -0600

    added api request system

--- Assets/Jira/JiraConnect.cs
+++ Assets/Jira/JiraConnect.cs
@@ -11,22 +11,23 @@ using TMPro;
 public class JiraConnect : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI jiraText;
+    [SerializeField] private TMP_InputField tokenInputField;
+    [SerializeField] private TMP_InputField emailInputField;
+    [SerializeField] private TMP_InputField APIInputField;
 
     private void Start()
     {
-        // https://adam-mcneil.atlassian.net/rest/api/latest/issue/Test?-H \"Authorization: HebzeuStL2EU43ulUo46C682\"
-
-        string baseURL = "https://adam-mcneil.atlassian.net/rest/";
-        string loginUsername = "adamwmcneil@gmail.com";
-        //string loginPassword = "something";
-        string loginToken = "ZCwd7guPdWjfJyobV0cE3F49";
-        // string loginAPI = "auth/latest/session/";
-        string getURL = "api/latest/search?name=yf";
+        // https://adam-mcneil.atlassian.net/rest/api/latest/issue/Test?-H 
+        // baseURL = "https://adam-mcneil.atlassian.net/rest/";
+        // getURL = "api/latest/search?name=yf";
 
 
-        MakeJiraRequest(baseURL + getURL, loginUsername, loginToken);
         //MakeJiraTicket(baseURL + getURL, loginUsername, loginToken);
-        Console.ReadLine();
+    }
+
+    public void OnRunButtonClick()
+    {
+        MakeJiraRequest(APIInputField.text, emailInputField.text, tokenInputField.text);
     }
 
     private void MakeJiraTicket(string URL, string username, string token)
@@ -55,16 +56,23 @@ public class JiraConnect : MonoBehaviour
     private void MakeJiraRequest(string URL, string username, string token)
     {
         // make data request
-        WebRequest issueRequest = WebRequest.Create(URL);
-        string encodedStr = Convert.ToBase64String(Encoding.GetEncoding("UTF-8").GetBytes(username + ":" + token));
-        issueRequest.Headers.Add("Authorization", "Basic " + encodedStr);
-        issueRequest.Method = "GET";
-        WebResponse webResponse = issueRequest.GetResponse();
-        Stream responseStream = webResponse.GetResponseStream();
-        StreamReader reader = new StreamReader(responseStream);
-        string response = reader.ReadToEnd();
-        jiraText.text = response;
-        Debug.Log(response);
+        try
+        {
+            WebRequest issueRequest = WebRequest.Create(URL);
+            string encodedStr = Convert.ToBase64String(Encoding.GetEncoding("UTF-8").GetBytes(username + ":" + token));
+            issueRequest.Headers.Add("Authorization", "Basic " + encodedStr);
+            issueRequest.Method = "GET";
+            WebResponse webResponse = issueRequest.GetResponse();
+            Stream responseStream = webResponse.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string response = reader.ReadToEnd();
+            jiraText.text = response;
+            Debug.Log(response);
+        }
+        catch (Exception ex)
+        {
+            jiraText.text = ex.Message;
+        }
     }
 }
 
